cd57e48c095b94c30cbb3eef7ab55d50472f85ac
Carl Bäckström <genzorg@gmail.com>
2017-08-14 22:07:39 +0200

    i3: Fixed both list projects scripts

--- roles/rofi/files/pycharm_projects.py
+++ roles/rofi/files/pycharm_projects.py
@@ -2,10 +2,11 @@
 from __future__ import print_function
 
 import os
+import sys
 import time
 
+from sh import pycharm, find
 import click
-from sh import locate, pycharm, find
 
 
 PROJECT_EXCLUDES = ['hoist-non-react-statics', 'multi-stage-sourcemap', 'corslite',
@@ -14,8 +15,15 @@ PROJECT_EXCLUDES = ['hoist-non-react-statics', 'multi-stage-sourcemap', 'corslit
 
 PYCHARM_PROJECTS_FILE = os.path.expanduser('~/.sublime_projects')
 
+
 def _get_parent_basename(path):
-    return os.path.basename(os.path.dirname(path))
+    return os.path.basename(os.path .dirname(path))
+
+
+def _update_db():
+    find(os.path.expanduser('~/development'), os.path.expanduser('~/bsdev'),
+                            '-type', 'd', '-name', '.idea', _out=PYCHARM_PROJECTS_FILE)
+
 
 def _process_output(line):
     """
@@ -28,16 +36,12 @@ def _process_output(line):
         print(project_filename)
 
 
-
 def _get_projects_on_disk():
     last_modified_epoch_time = os.path.getmtime(PYCHARM_PROJECTS_FILE)
     epoch_time = int(time.time())
 
     if (epoch_time - last_modified_epoch_time) > 300:
-        print("Updating")
-
-        find(os.path.expanduser('~/development'), os.path.expanduser('~/bsdev'),
-                                '-type', 'd', '-name', '.idea', _out=PYCHARM_PROJECTS_FILE)
+        _update_db()
 
     with open(PYCHARM_PROJECTS_FILE) as project_file:
         projects_on_disk = project_file.readlines()
@@ -45,10 +49,15 @@ def _get_projects_on_disk():
     return projects_on_disk
 
 
-
 @click.command()
 @click.argument('project', nargs=-1)
-def sublime_projects(project):
+@click.option('--update/--no-update', default=False)
+def sublime_projects(project, update):
+
+    if update:
+        _update_db()
+        sys.exit()
+
     if not project:
         for project_on_disk in _get_projects_on_disk():
             _process_output(project_on_disk)
@@ -56,6 +65,7 @@ def sublime_projects(project):
         for project_on_disk in _get_projects_on_disk():
             if project[0] == _get_parent_basename(project_on_disk):
                 pycharm(os.path.dirname(project_on_disk), _bg=True)
+
 
 if __name__ == '__main__':
     sublime_projects()
--- roles/rofi/files/sublime_projects.py
+++ roles/rofi/files/sublime_projects.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python2
 from __future__ import print_function
 
-
 import os
-from datetime import datetime
+import sys
 import time
 
-from sh import locate, subl, find
+from sh import subl, find
 import click
 
 
+PROJECT_EXCLUDES = []
+
 SUBLIME_PROJECTS_FILE = os.path.expanduser('~/.sublime_projects')
+
+
+def _get_parent_basename(path):
+    return os.path.basename(os.path .dirname(path))
+
+
+def _update_db():
+    find(os.path.expanduser('~/development'), os.path.expanduser('~/bsdev'),
+                            '-name', '*.sublime-project', _out=SUBLIME_PROJECTS_FILE)
 
 
 def _process_output(line):
@@ -18,10 +28,11 @@ def _process_output(line):
     Callback for printing output from sh commands
     :param line:
     """
+    project_filename = os.path.basename(line.rstrip('\n'))
 
-    sublime_project_filename = os.path.basename(line.rstrip('\n'))
-    filename, extension = os.path.splitext(sublime_project_filename)
-    print(filename)
+    if project_filename not in PROJECT_EXCLUDES:
+    	filename, extension = os.path.splitext(sublime_project_filename)
+	print(filename)
 
 
 def _get_projects_on_disk():
@@ -29,33 +40,30 @@ def _get_projects_on_disk():
     epoch_time = int(time.time())
 
     if (epoch_time - last_modified_epoch_time) > 300:
-        print("Updating")
-
-        find(os.path.expanduser('~/development'), os.path.expanduser('~/bsdev'),
-                                '-name', '*.sublime-project', _out=SUBLIME_PROJECTS_FILE)
+        _update_db()
 
     with open(SUBLIME_PROJECTS_FILE) as project_file:
         projects_on_disk = project_file.readlines()
+
     return projects_on_disk
-
-
-def _get_parent_basename(path):
-    return os.path.basename(os.path.dirname(path))
 
 
 @click.command()
 @click.argument('project', nargs=-1)
-def sublime_projects(project):
+@click.option('--update/--no-update', default=False)
+def sublime_projects(project, update):
+
+    if update:
+        _update_db()
+        sys.exit()
+
     if not project:
         for project_on_disk in _get_projects_on_disk():
             _process_output(project_on_disk)
-
     else:
         for project_on_disk in _get_projects_on_disk():
             if project[0] == _get_parent_basename(project_on_disk):
                 subl(os.path.dirname(project_on_disk))
-
-        subl(project_on_disk)
 
 
 if __name__ == '__main__':
