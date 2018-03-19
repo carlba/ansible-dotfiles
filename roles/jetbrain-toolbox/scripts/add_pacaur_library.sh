de10b060836612f9535977e9f0f40df5dc64fcf1
Carl Bäckström <genzorg@gmail.com>
2018-03-19 02:09:20 +0100

    removed submodules

--- roles/jetbrain-toolbox/scripts/add_pacaur_library.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ../
-mkdir -p library/external_modules
-git submodule add git://github.com/cdown/ansible-aur.git library/external_modules/ansible-aur
-ln -s external_modules/ansible-aur/aur library/aur
